9b70695ced36747febc0c65fd6d2238674412981
Filip Cudny <filip96zx@gmail.com>
2022-02-20 19:20:52 +0100

    added global styles and typography

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
+      <h1>heading smalplelekr</h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis facilis distinctio pariatur optio nam, possimus aperiam sit qui molestiae vitae, expedita atque, excepturi eius placeat nobis incidunt assumenda. Repellendus, numquam.
     </div>
   );
 };
---
+++ src/globalStyles/global.ts
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Bellefair&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Barlow&display=swap');
+
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  --main-color: rgb(11,13,23);
+  --secondary-color: rgb(208,214,249);
+  --font-color: rgb(255,255,255);
+}
+
+body {
+  font-family: 'Barlow', sans-serif;
+  color: var(--font-color);
+}
+
+h1, h2, h3, h4 {
+  font-family: 'Bellefair', serif;
+  font-weight: 400;
+  font-size: 156px;
+}
+
+h2 {
+  font-size: 100px;
+}
+
+h3 {
+  font-size: 56px;
+}
+
+h4 {
+  font-size: 32px;
+}
+
+h5 {
+  font-family: 'Barlow Condensed', sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+  letter-spacing: 4.75px;
+}
+
+.subheading-1 {
+  font-family: 'Bellefair', serif;
+  font-size: 28px;
+}
+
+.subheading-2 {
+  font-family: 'Barlow Condensed', sans-serif;
+  font-size: 14px;
+  letter-spacing: 2.35px;
+}
+
+nav {
+  font-family: 'Barlow Condensed', sans-serif;
+  font-size: 16x;
+  letter-spacing: 2.7px;
+}
+
+`;
+
+export default GlobalStyle;
