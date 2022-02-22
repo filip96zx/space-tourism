7b129f68d9620b9e302cb78221cf64030062c77d
Filip Cudny <filip96zx@gmail.com>
2022-02-22 15:05:08 +0100

    destination model updated

--- src/models/destination.model.ts
+++ src/models/destination.model.ts
@@ -1,6 +1,11 @@
 export default interface Destination {
+  id: number;
   title: string;
   description: string;
   distance: string;
   travelTime: string;
+  imagePaths: {
+    webp: string;
+    png: string;
+  };
 }
