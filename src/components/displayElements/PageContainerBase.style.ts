24805b346e75b1ca2caeaa75709ab7ee2697c1c0
Filip Cudny <filip96zx@gmail.com>
2022-03-01 21:12:45 +0100

    ref: removing unnecesary styling

--- src/components/displayElements/PageContainerBase.style.ts
+++ src/components/displayElements/PageContainerBase.style.ts
@@ -3,7 +3,6 @@ import { css } from 'styled-components';
 const PageContainerBase = (mobileBackground?: string, tabletBackground?: string, desktopBackground?: string) => css`
   background-image: url(${mobileBackground});
   background-size: cover;
-  //width: 100vw;
   overflow-x: scroll;
   display: flex;
   scroll-snap-type: x mandatory;
--- src/globalStyles/global.ts
+++ src/globalStyles/global.ts
@@ -18,26 +18,13 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Barlow', sans-serif;
     color: var(--font-color);
     background-color: var(--main-color);
-    //overflow-y: hidden;
   }
 
   h1, h2, h3, h4 {
     font-family: 'Bellefair', serif;
     font-weight: 400;
-    font-size: 156px;
   }
 
-  h2 {
-    font-size: 100px;
-  }
-
-  h3 {
-    font-size: 56px;
-  }
-
-  h4 {
-    font-size: 32px;
-  }
 
   h5 {
     font-family: 'Barlow Condensed', sans-serif;
