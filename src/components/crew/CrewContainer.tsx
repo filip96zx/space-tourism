fe574f5e701175ab83196536943363465ecc90bd
Filip Cudny <filip.cudny@enigma.com.pl>
2022-03-06 18:28:31 +0100

    removed imports

--- src/components/crew/CrewContainer.tsx
+++ src/components/crew/CrewContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import useSlider from '../../helpers/slider';
 
 import DouglasPng from '../../assets/crew/image-douglas-hurley.png';
--- src/components/destination/DestinationContainer.tsx
+++ src/components/destination/DestinationContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Destination from '../../models/destination.model';
 import DestinationComponent from './DestinationComponent';
 import DestinationContainerStyled from './DestinationContainer.style';
